a38dfec108813f3bc32c871b346a6e99083c9479
KatAlves <KatAlves@users.noreply.github.com>
2021-04-21 23:14:43 +0100

    Heading

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './owl.jpg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      
+Dictionary     
+    
+      </header>
+    </div>
+  );
+}
+

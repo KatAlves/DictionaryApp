b41541dd186c5864ed7fa143e39e1dea4e240ef5
KatAlves <KatAlves@users.noreply.github.com>
2021-04-25 00:54:46 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,8 +7,7 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       
-Dictionary     
-    tesT
+Dictionary   
       </header>
     </div>
   );

f6b6c37a02c0499be5bfafd5b6da5c7f59f87cc8
KatAlves <kath.sama@gmail.com>
2021-05-04 19:05:17 +0100

    Call to Dictionary API
    https://dictionaryapi.dev/

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
 let [keyWord, setkeyWord] = useState("");
 
+function handleResponse(response){
+console.log(response.data[0]);
+}
+
+
   function Search(event) {
       event.preventDefault();
 alert(`Searching for ${keyWord}`);
+
+let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
+axios.get(apiUrl).then(handleResponse);
 
   }
 

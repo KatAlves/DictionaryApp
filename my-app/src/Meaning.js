2bbdb40ff8d5c0d7691c097eca1c459fa15ac796
KatAlves <kath.sama@gmail.com>
2021-05-10 20:27:25 +0100

    Reduce Synonyms to 4.

--- my-app/src/Meaning.js
+++ my-app/src/Meaning.js
@@ -5,26 +5,19 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-   
-        {props.meaning.definitions.map(function (definition, index) {
-        if(index < 3) {
-            return (
 
-            <div key={index}>
-              <p>
-              <strong>Definition:</strong>  {definition.definition}
-                <br />
-                <em>{definition.example}</em>
-              </p>
-              <Synonyms synonyms={definition.synonyms}/>
-            </div>
-          );
-           } else {
-              return null;
-          }
-
-        })}
-
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong> {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
   );
 }
--- my-app/src/Synonyms.js
+++ my-app/src/Synonyms.js
@@ -5,7 +5,11 @@ export default function Synonyms(props) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+          if (index < 4) {
+            return <li key={index}>{synonym}</li>;
+          } else {
+            return null;
+          }
         })}
       </ul>
     );
